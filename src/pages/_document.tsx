7dbaebfe6295bf62f0509227c53f55f342495981
JoaoVSouto <joaovitorss99@hotmail.com>
2021-05-26 18:31:20 -0300

    Configure Google fonts and favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,43 @@
-import Document from 'next/document';
+import Document, { Html, Head, NextScript, Main } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+
+          <meta name="theme-color" content="#1a1d23" />
+        </Head>
+
+        <body>
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }

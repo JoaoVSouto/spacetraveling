4800bc41877138d4c9a1e8eeedfa5dfa71dfdabb
JoaoVSouto <joaovitorss99@hotmail.com>
2021-05-29 15:45:33 -0300

    Create and style post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
-import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
@@ -26,9 +27,60 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Lorem Ipsum | spacetraveling.</title>
+      </Head>
+
+      <img
+        src="https://hallcpas.com/wp-content/uploads/2018/01/mountains-min-1440x400.jpg"
+        alt="Mountains"
+        className={styles.banner}
+      />
+
+      <main className={styles.container}>
+        <h1>Criando um app CRA do zero</h1>
+
+        <div className={styles.postInfo}>
+          <time>
+            <FiCalendar size={20} />
+            15 Mar 2021
+          </time>
+          <span>
+            <FiUser size={20} />
+            João Vítor
+          </span>
+          <span>
+            <FiClock size={20} /> 4 min
+          </span>
+        </div>
+
+        <div className={styles.content}>
+          <h3>Proin et varius</h3>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo,
+            explicabo!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            nisi unde dolorum nihil, sequi consequuntur pariatur doloribus quam
+            quis molestias. Facere obcaecati iure, sint voluptas neque officia
+            optio! A ullam sed maxime maiores quisquam quia quis, eaque
+            veritatis sunt laboriosam saepe iste recusandae tempora culpa quo
+            expedita doloremque beatae perspiciatis.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            iure quam dolore animi vel a.
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
